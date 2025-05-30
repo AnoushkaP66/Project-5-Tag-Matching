81f0b89c11aa98cb5339a44d7938b583ed40e275
AnoushkaP66 <anoushkapoojary@gmail.com>
2025-05-30 16:54:12 -0400

    Update MyStack.java

--- MyStack.java
+++ MyStack.java
@@ -1,13 +1,3 @@
-/***
- * @author Anoushka Poojary
- * CMSC 256 - Intro to Data Structures Section 901
- * Project 5 - Tag Matching
- * @version 03/28/2025
- * This project implements the usage of the Node class and the Stack interface to check for matching HTML tags. The methods
- * in the Stack interface are used for a method that will process an HTML file and will check to see if the tags are properly
- * balanced. Exceptions and errors are accounted for in this project.
- */
-
 package cmsc256;
 
 import java.io.File;
